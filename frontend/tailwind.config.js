92b343b7209c9cef1df196f35afc40d63615f643
Nikhil Virdi <nkvir2468@gmail.com>
2025-11-18 10:49:33 +0530

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "cyber-blue": "#128CFF",
+      },
+      fontFamily: {
+        heading: ["Space Grotesk", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
